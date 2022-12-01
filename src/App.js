a4cdf0768d03194dc83daab0c5d617b1765f04b6
Artur <arkhannanov@gmail.com>
2022-12-01 19:14:31 +0300

    changes initial route

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import './App.css';
 import Header from "./Components/Header/Header";
 import CarsWrapper from "./Components/CardsWrapper/CarsWrapper";
-import {BrowserRouter, Route, Routes, useHistory} from "react-router-dom";
+import { Route, Routes} from "react-router-dom";
 import Basket from "./Components/Basket/Basket";
-import {useEffect} from "react";
 
 function App() {
 
-    const history = useHistory()
-
-    useEffect(()=> {
-      history.push('/catalog')
-    },[])
     return (
-        <BrowserRouter>
-            <div className="App">
-                <Header/>
-                <Routes>
-                    <Route path="/catalog" element={<CarsWrapper/>}/>
-                    <Route path="/basket" element={<Basket/>}/>
-                </Routes>
-            </div>
-        </BrowserRouter>
+        <div className="App">
+            <Header/>
+            <Routes>
+                <Route path="/catalog" element={<CarsWrapper/>}/>
+                <Route path="/basket" element={<Basket/>}/>
+            </Routes>
+        </div>
     );
 }
 
